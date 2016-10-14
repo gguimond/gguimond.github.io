c87dd86763723a07c62aa6e0462ebe68e9adc974
Guillaume Guimond <guillaume.guimond@orange.com>
2016-10-14 14:34:42 +0200

    init repo

---
+++ sw.js
@@ -0,0 +1,9 @@
+'use strict';
+
+importScripts('/js/sw-toolbox.js');
+
+toolbox.router.default = toolbox.networkFirst;
+toolbox.options.networkTimeoutSeconds = 5;
+
+toolbox.precache(['/index.html', '/js/app.js', '/js/vendors.js', '/css/app.css', '/css/vendors.css']);
+toolbox.router.any('/images/*', toolbox.fastest);
